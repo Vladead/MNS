70f0654acb0da41babb2f15ebdb3f62d5e7a097b
Vladead <vladead8@yandex.ru>
2021-09-20 20:55:51 +0300

    First lab

---
+++ FRQVS/FrqvsDlg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FRQVS
+{
+    public partial class FrqvsDlg : Form
+    {
+        public FrqvsDlg()
+        {
+            InitializeComponent();
+        }
+
+        private void ID_PRIV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ID_EXIT_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
